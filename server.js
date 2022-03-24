de24b24935c562df307c8360d5a9458cdc184c06
Roger Wells <rsw359@gmail.com>
2022-03-24 11:36:45 +0900

    setup api weather request

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 'use strict';
-// const e = require('express');
-const express = require('express');
-require('./.env');
+
 require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
 let weatherData = require('./data/weather.json');
 // let data = require('data/weather.json');
 
 //use
 const app = express();
+app.use(cors());
 
 const PORT = process.env.PORT || 3002;
 
 // Routes
 
-app.get('/weather', (request, response) => {
-  let searchQuery = request.query.city_name;
-  let cityData = weatherData.find(city => city.city_name.toUpperCase() === searchQuery.toUpperCase());
+app.get('/weather', async (request, response) => {
 
-  let dataArray= cityData.data.map(dailyCityData => new Forecast(dailyCityData));
+  let searchQueryLat = request.query.lat
+
+  let searchQueryLon = request.query.lon
+
+  let url = `http://api.weatherbit.io/v2.0/current?lat=${searchQueryLat}&lon=${searchQueryLon}&key=${process.env.WEATHER_API_KEY}`
+
+  let weatherBitData = await axios.get(url);
+  console.log(weatherBitData.data);
+  let dataArray = weatherBitData.data.data.map(dailyCityData => new Forecast(dailyCityData));
   response.send(dataArray);
+
 });
 
 app.get('*', (request, response) => {
@@ -30,9 +39,10 @@ app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 
 
 class Forecast {
-  constructor(dailyCityData){
+  constructor(dailyCityData) {
 
     this.date = dailyCityData.valid_date;
     this.description = dailyCityData.weather.description;
+
   }
 }
